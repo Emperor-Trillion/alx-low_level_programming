e52e2dd75f7bcb4a52d02581ebd5ca45c03ce151
Sunday Sanni <sundayysanni@gmail.com>
2022-10-21 03:57:29 -0700

    AgainD

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 
 {
 	char c;
@@ -18,7 +18,6 @@ int main (void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("Size of a float: %zu byte(s\n)", (unsigned long)sizeof(f));
-	
-	return(0);
+	printf("Size of a float: %lu byte(s\n)", (unsigned long)sizeof(f));
+	return (0);
 }
