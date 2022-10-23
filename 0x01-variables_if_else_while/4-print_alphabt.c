7323f18e2cb3e874461c03540128b2a589c2fca2
Sunday Sanni <sundayysanni@gmail.com>
2022-10-23 09:49:12 -0700

    Initiative taken

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+	if (c != 'e' && c != 'q')
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
