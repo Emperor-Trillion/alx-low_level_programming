5e8c59109e3183bd1654870ceb38ab7f3ca68dc4
Sunday Sanni <sundayysanni@gmail.com>
2022-10-24 10:22:04 -0700

    True Understanding pays

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,11 @@ int c;
 for (c = 48; c <= 57; c++)
 {
 	putchar(c);
-	if (c != 57)
-	putchar(44);
-	putchar(32);
+	if (c!=57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 }
 putchar('\n');
 return (0);
