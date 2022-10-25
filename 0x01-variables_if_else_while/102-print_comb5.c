711539ed5c6d8500142ce3422b20b41eba643131
Sunday Sanni <sundayysanni@gmail.com>
2022-10-25 08:49:29 -0700

    Trial

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,85 @@
-#include <stdio.h>
-/* more headers goes there */
+#include <stdio.h>                                                                                                                      
 
-/* betty style doc for function main goes there */
-/**
- * main -Entry point
+/* more headers goes there */                                                                                                           
+
+                                                                                                                                        
+
+/* betty style doc for function main goes there */                                                                                      
+
+/**                                                                                                                                     
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-int a;
-int b;
-int c;
-int d;
+ *  * main -Entry point                                                                                                                    
+ *
+ *   *                                                                                                                                      
+ *
+ *    * Return: Always 0 (Success)                                                                                                           
+ *
+ *     */                                                                                                                     
 
-for (a = 48; a <= 57; a++)
-{
-	for (b = 48; b <= 57; b++)
-	{
-		for (c =48; c <= 57; c++)
-		{
-			for (d = 49; d <= 57; d++)
-			{
-				if (((d * 1000) + (c * 100) + (b *10) + (a *1)) > ((a * 1000) + (b * 100) + (c * 10) + (d * 1)))
-					{
-					putchar(a);
-					putchar(b);
-					putchar(32);
-					putchar(c);
-					putchar(d);
-					if ((a + b + c + d) != (57 + 56 + 57 + 57))
-					putchar(44);
-					putchar(32);
-					}	
-			}
-		}	
-	}
-}
-putchar('\n');
-return (0);
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+	int a;                                                                                                                                  
+
+	int b;                                                                                                                                  
+
+	int c;                                                                                                                                  
+
+	int d;                                                                                                                                
+
+	                                                                                                                                        
+
+	for (a = 48; a <= 57; a++)                                                                                                              
+
+	{                                                                                                                                     
+
+		        for (b = 48; b <= 57; b++)                                                                                                     
+
+				        {                                                                                                                              
+
+						                for (c =48; c <= 57; c++)                                                                                              
+
+									                {                                                                                                                     
+
+												                        for (d = 49; d <= 57; d++)
+
+																                        {             
+
+
+
+																				                                if (((d * 1000) + (c * 100) + (b * 10) + (a * 1)) > ((a * 1000) + (b * 100) + (c * 10) + (d * 1)))
+
+																									                                {                                                                                                 
+
+																														                                        putchar(a);                                                                                     
+
+																																			                                        putchar(b);                                                                                     
+
+																																								                                        putchar(32);                                                                                   
+
+																																													                                        putchar(c);                                                                                     
+
+																																																		                                        putchar(d);                                                                
+
+																																																							                                        if ((a + b + c + d) != (57 + 56 + 57 + 57))
+
+																																																													                                        { putchar(44);                                                                                    
+
+																																																																			                                        putchar(32); }
+
+																																																												                                }                                                                               
+
+																								                        }                                                                                                                                                                                                            
+
+															                }                                                                                                                     
+
+								        }                                                                                                                              
+
+	}                                                                                                                                   
+
+	putchar('\n');                                                                                                                       
+
+	return (0);                                                                                                                            
+
 }
