4d12894ef865b94877df19942849bfe3b1f57d53
Sunday Sanni <sundayysanni@gmail.com>
2022-10-28 00:00:09 -0700

    A

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int print_alphabet(void)
+{
+	for (int a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int print_alphabet(void);
+int _putchar(char c);
+
+#endif
