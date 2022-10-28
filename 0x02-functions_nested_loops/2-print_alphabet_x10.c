83450280da7483707733a1d43fa4b1eae7347cc7
Sunday Sanni <sundayysanni@gmail.com>
2022-10-28 06:54:19 -0700

    A

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Description
+ *
+ * Return: Always 0.
+ */
+int print_alphabet_x10(void)
+
+{
+	int a;
+	int aa;
+	
+	for (aa = 0; aa <= 9; aa++) 
+	{
+		for (a = 97; a <= 122; a++)
+		{
+		putchar(a);
+		}
+		putchar('\n');
+	}
+return (0);
+}
