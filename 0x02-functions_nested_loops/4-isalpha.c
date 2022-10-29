0c41b9acc32c5eace628513e864590365b68ac58
Sunday Sanni <sundayysanni@gmail.com>
2022-10-29 09:07:14 -0700

    Adding the function to check alphabets for the first time

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * isalpha - checks is input is an alphabet
+ * @c: c is all spectrum of the ascii character to check
+ *
+ * Return: '1' if input is an alphabet and '0' if not.
+ */
+int isalpha(int c)
+{
+	if ((c >= 65) && (c <= 122))
+	{
+		c = 1;
+	}
+	else
+	{
+		c = 0;
+	}
+return (c);
+}
