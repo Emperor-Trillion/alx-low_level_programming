4396cfe3124ea42f71842255885e50b2b031311b
Sunday Sanni <sundayysanni@gmail.com>
2022-10-29 09:45:59 -0700

    Sign and Number printed based on input

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+i#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - checks if input is a positive, negative or zero
+ * @n: n is all spectrum of the ascii character to check
+ *
+ * Return: '1', '0', '-1' for postive, zero, and negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		c = 1;
+	}
+	else if (n = 0)
+	{
+		putchar('0');
+		c = 0;
+	}
+	else
+	{
+		putchar('-');
+		c = '/'
+	}
+return (c);
+}
