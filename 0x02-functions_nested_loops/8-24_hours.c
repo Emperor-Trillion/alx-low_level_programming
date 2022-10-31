e3a822deeb9d09cad818c9be51cdfbc776c4ea85
Sunday Sanni <sundayysanni@gmail.com>
2022-10-31 10:02:18 -0700

    Hopeful

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,31 +21,31 @@ int jack_bauer(void)
 	for (a = 48; a <= 49; a++)
 	{
 		for (b = 48; b <= 57; b++)
-        	{
-            		for (c = 48; c <= 53; c++)
+		{
+			for (c = 48; c <= 53; c++)
 			{
 				for (d = 48; d <= 57; d++)
-                		{
-                    			putchar(a);
-                    			putchar(b);
-                    			putchar(':');
-                    			putchar(c);
-                    			putchar(d);
-                    			putchar('\n');
+				{
+					putchar(a);
+					putchar(b);
+					putchar(':');
+					putchar(c);
+					putchar(d);
+					putchar('\n');
 					if (a == '1' && b == '9' && c == '5' && d == '9')
-                    			{
-                        			for (f = 48; f <= 51; f++)
-                        			{
-                            				for (g = 48; g <= 53; g++)
-                            				{
-                                				for (h = 48; h <= 57; h++)
-                                				{
-                                    					putchar(e);
-                                    					putchar(f);
-                                    					putchar(':');
-                                    					putchar(g);
-                                    					putchar(h);
-                                    					putchar('\n');
+					{
+						for (f = 48; f <= 51; f++)
+						{
+							for (g = 48; g <= 53; g++)
+							{
+								for (h = 48; h <= 57; h++)
+								{
+									putchar(e);
+									putchar(f);
+									putchar(':');
+									putchar(g);
+									putchar(h);
+									putchar('\n');
 								}
 							}
 						}
