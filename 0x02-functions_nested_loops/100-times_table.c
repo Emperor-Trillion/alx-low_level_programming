bcebf889a86f73f59a2cccbff1584c1ad547fa16
Sunday Sanni <sundayysanni@gmail.com>
2022-11-02 10:26:35 -0700

    Advanced 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints times table of inputed number
+ *
+ * Return: Always 0.
+ */
+int print_times_table(int n)
+
+{
+	int a;
+	int b;
+	int c;
+
+	if ((n > 0) && (n <= 15))
+	{
+		for (a = 0; a < (n + 1); a++)
+		{
+			for (b = 0; b < (n + 1); b++)
+			{
+				c = a + b;
+				if (b == 0)
+				{
+					putchar('0');
+				}
+				else if (c > 9 && c <= 99)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					printf("%d", c);
+				}
+				else if (c > 99)
+				{
+					putchar(',');
+					putchar(' ');
+					printf("%d", c);
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					printf("%d", c);
+				}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
