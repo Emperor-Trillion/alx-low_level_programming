1bac5bff5d56a90af7692c7e07785480bfb41bf6
Sunday Sanni <sundayysanni@gmail.com>
2022-11-03 07:53:01 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int b = 1;
+	int c = 2;
+	int d;
+	int t;
+
+	printf("%d, ", b);
+	for (d = 0; d <= 49; d++)
+	{
+		printf("%d, ", c);
+		t = b + c;
+		b = c;
+		c = t;
+	}
+	return (0);
+}
