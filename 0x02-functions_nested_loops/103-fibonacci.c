87e0e3dcce69bde67a28ccc80510939a6e622052
Sunday Sanni <sundayysanni@gmail.com>
2022-11-04 14:56:05 -0700

    A

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,18 +7,22 @@
  */
 int main(void)
 {
-	long long int b = 1;
-	long long int c = 2;
-	int d;
-	long long int t;
+	long int b = 1;
+	long int c = 2;
+	long int t;
+	long int Total = 0;
 
-	printf("%llu, ", b);
-	for (d = 0; d <= 49; d++)
+	for (c = 2; c <= 4000000; c++)
 	{
-		printf("%llu, ", c);
+		if (c % 2 == 0)
+		{
+			Total = Total + c;
+		}
 		t = b + c;
 		b = c;
 		c = t;
 	}
+	printf("%lu", Total);
+	putchar('\n');
 	return (0);
 }
