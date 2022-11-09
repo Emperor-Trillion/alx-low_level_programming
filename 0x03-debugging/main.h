663401d874809d947521df870c142b6a3ec46649
Sunday Sanni <sundayysanni@gmail.com>
2022-11-09 09:44:56 -0800

    A

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 
-positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+	positive_or_negative(int i);
+	largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
