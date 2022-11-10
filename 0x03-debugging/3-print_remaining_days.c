505ba396e24c29864eb8bb36e6dec84fcd5ef288
Sunday Sanni <sundayysanni@gmail.com>
2022-11-10 07:41:06 -0800

    A

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,19 +12,12 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-	{
 		if (month > 2 && day >= 60)
 		{
-	
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
-	else
-	{
-		if (month == 2 && (year % 4 != 0) && day == 60)
+		else if (month == 2 && (year % 4 != 0) && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
@@ -38,5 +31,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
 }
