636bd3ab2f911bbd815638c4a40504cee051eb11
Sunday Sanni <sundayysanni@gmail.com>
2022-11-11 08:01:08 -0800

    A

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints set of numbers with exception of some
+ * takes not input
+ *
+ * Return: returns no output
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 || a != 52)
+		{
+		_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
