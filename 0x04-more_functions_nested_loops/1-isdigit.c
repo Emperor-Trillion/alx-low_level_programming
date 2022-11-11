afefed2e9f8a427d72b13711856fc60198446977
Sunday Sanni <sundayysanni@gmail.com>
2022-11-11 07:17:53 -0800

    A

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks whether input is a digit or not
+ * @c: the input
+ * Return: 1 for positive and 0 for negative
+ */
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 59)
+	{
+		return (1);
+		putchar('\n');
+	}
+	else
+	{
+		return (0);
+		putchar('\n');
+	}
+}
