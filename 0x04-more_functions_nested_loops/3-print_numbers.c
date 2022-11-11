c399b5d1c2e76fd4aa535f34ffad998e54171f6c
Sunday Sanni <sundayysanni@gmail.com>
2022-11-11 07:43:03 -0800

    A

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints set of numbers
+ * takes not input
+ *
+ * Return: returns no output
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
