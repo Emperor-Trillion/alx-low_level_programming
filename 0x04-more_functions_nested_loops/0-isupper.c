7601502b8c0c949ebe52b4a1b3228306e8768374
Sunday Sanni <sundayysanni@gmail.com>
2022-11-11 04:00:52 -0800

    A

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - returns whther input is upper case or not
+ * @c: the input
+ * Return: 1 for positive and 0 for negative
+ */
+
+int _isupper (int c)
+{
+
+	if (c >= 65 && <= 90 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
