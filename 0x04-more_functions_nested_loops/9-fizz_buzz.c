72838a006138e3abe3c43788887b9c961a7e1f36
Sunday Sanni <sundayysanni@gmail.com>
2022-11-12 07:38:37 -0800

    A

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,29 +5,29 @@
  *
  * Return: returns 0
  */
-int main (void)
+int main(void)
 {
 	int a;
 
 	for (a = 1; a < 101; a++)
+	{
+		if (a % 3 == 0)
 		{
-			if (a % 3 == 0)
-			{	
-				printf("Fizz ");
-			}
-			else if (a % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if ((a % 3 == 0) && (a % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			else
-			{
-				printf("%d ", a);
-			}
+			printf("Fizz ");
 		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
 	printf("\n");
 	return (0);
 }
